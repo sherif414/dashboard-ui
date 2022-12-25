3f5d8456045d793c82565e842541110ec38dfae4
shareef <creazyhell@gmail.com>
2022-12-25 15:31:16 +0200

    refactor: changed the ui from outlined to filled

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,43 +1,41 @@
 <template>
-  <label class="flex flex-col gap-y-1 relative">
-    <div>
+  <label :class="classes" class="flex flex-col gap-y-1 relative">
+    <div class="capitalize typo-sm">
       {{ label }}
     </div>
-    <div
-      class="px-3 py-1 rounded flex items-center gap-x-3 outline surface-1"
-      :class="[
-        {
-          'outline-indigo-4 dark:outline-violet-4 outline-2': isFocused,
-          'outline-gray-3 dark:outline-dark-3 outline-1': !isFocused && !isInvalid,
-          'outline-error outline-1 text-error': isInvalid && !isFocused,
-        },
-        classes,
-      ]"
-    >
-      <div class="w-4 h-4 flex justify-center items-center" v-if="$slots.prepend">
+    <div :class="{ 'text-error': isInvalid }" class="relative w-full">
+      <input
+        ref="inputEl"
+        :class="[
+          $slots.prepend ? 'pl-11' : 'pl-4',
+          $slots.append ? 'pr-11' : 'pr-4',
+          isInvalid ? 'bg-opacity-10! bg-error!' : 'surface-2',
+        ]"
+        class="h-12 rounded-md outline-none outline-offset-0! focus:(dark:outline-violet outline-indigo-4) w-full resize-none"
+        @[eventType]="handleEmit"
+        @invalid="isInvalid = true"
+        @focus="isInvalid = false"
+        @blur="validate"
+        :type="type"
+        :value="modelValue"
+        v-bind="$attrs"
+      />
+      <div
+        class="absolute h-full w-12 grid place-items-center left-0 top-0 pointer-events-none [&_svg]:(w-5 h-5)"
+        v-if="$slots.prepend"
+      >
         <slot name="prepend" />
       </div>
-      <input
-        ref="el"
-        class="h-2rem focus:outline-none active:outline-none grow bg-inherit w-full resize-none border-none"
-        @focus="handleFocus"
-        @blur="handleBlur"
-        @[eventType]="handleEmit"
-        :placeholder="placeholder"
-        :value="modelValue"
-        :required="required"
-        :disabled="disabled"
-        :type="type"
-      />
-      <div v-if="$slots.append">
+      <div
+        class="absolute h-full w-12 grid place-items-center right-0 top-0 pointer-events-none [&_svg]:(w-5 h-5)"
+        v-if="$slots.append"
+      >
         <slot name="append" />
       </div>
+      <div v-show="isInvalid" class="text-error absolute top-110% left-0 text-10px">
+        {{ _errorMsg ?? 'invalid input' }}
+      </div>
     </div>
-    <span
-      class="duration-300 transition-opacity text-error absolute bottom--5 left-0 text-10px"
-      :class="[isInvalid && !isFocused ? 'opacity-100' : 'opacity-0 pointer-events-none']"
-      >{{ errorMsg ?? 'this field is required' }}</span
-    >
   </label>
 </template>
 
@@ -45,23 +43,17 @@
 interface Props {
   label?: string
   type?: 'text' | 'email' | 'password' | 'date' | 'number' | 'tel' | 'time' | 'url' | 'search'
-  disabled?: boolean
   modelValue?: string | number | null
   modelModifiers?: { noTrim?: boolean; noLazy?: boolean; isNumber?: boolean }
-  placeholder?: string
-  required?: boolean
   classes?: string
-  invalid?: boolean
-  errorMsg?: string
 }
 
-const { type = 'text', modelValue, required, invalid, modelModifiers } = defineProps<Props>()
-
-let eventType = $computed(() => {
-  return modelModifiers?.noLazy ? 'input' : 'change'
-})
-const el = $ref<HTMLInputElement | null>(null)
+const { type = 'text', modelValue, modelModifiers = {} } = defineProps<Props>()
 const emits = defineEmits(['update:modelValue'])
+const inputEl = $ref<HTMLInputElement | null>(null)
+
+let eventType = $computed(() => (modelModifiers?.noLazy ? 'input' : 'change'))
+let isInvalid = $ref(false)
 
 function handleEmit(e: Event) {
   let value: string | number | undefined = (e.target as HTMLInputElement)?.value
@@ -71,25 +63,15 @@ function handleEmit(e: Event) {
   }
 
   if (type === 'number' || modelModifiers?.isNumber) {
-    value = isNaN(parseFloat(value)) ? undefined : parseFloat(value)
+    value = parseFloat(value) || undefined
   }
 
   emits('update:modelValue', value)
 }
 
-let isFocused = $ref(false)
-let _invalid = $ref(false)
-let isInvalid = $computed(() => invalid || _invalid)
-
-function handleBlur() {
-  isFocused = false
-  if (required && !modelValue) {
-    _invalid = true
-  }
-}
-
-function handleFocus() {
-  isFocused = true
-  _invalid = false
+let _errorMsg = $ref<string>()
+function validate() {
+  _errorMsg = inputEl?.validationMessage
+  inputEl?.checkValidity()
 }
 </script>
